2f33c2d074c10dcd1d5fdb6a74efeaa6668a9cb9
HNLNHAT <nhathnlps22039@fpt.edu.vn>
2024-06-16 20:02:42 +0700

    Nhat
    16/06/2023

---
+++ src/screens/Login/SplashSreen.tsx
@@ -0,0 +1,14 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SplashSreen = () => {
+  return (
+    <View style={{backgroundColor: 'white', width: '100%', height:'100%',justifyContent:'center', alignContent: 'center', alignItems:'center'}}>
+      <Image source={require('../../asset/image/logolata.png')}/>
+    </View>
+  )
+}
+
+export default SplashSreen
+
+const styles = StyleSheet.create({})
