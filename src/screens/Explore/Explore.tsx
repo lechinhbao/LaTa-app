07405547eb970eb7447bbe03f43aa6bfe87fdc17
Sjllage <93598036+Sjllage@users.noreply.github.com>
2024-07-03 01:13:44 +0700

    Kiet commit 3/7

--- src/screens/Explore/Explore.tsx
+++ src/screens/Explore/Explore.tsx
@@ -13,12 +13,15 @@ import {
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons'
 import { CompositeNavigationProp, NavigationProp, useIsFocused, useNavigation } from '@react-navigation/native';
+import { RootStackParamListHome, RootStackScreenEnumHome } from '../../component/Root/RootStackHome';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootTabParamList } from '../../component/BottomNavigation/RootTab/RootTab';
 import { HEIGHT, PADDING_HORIZONTAL, PADDING_TOP, WIDTH } from '../../utilities/utility';
 import { COLORS } from '../../utilities';
+import AxiosInstance from '../../Axios/Axios';
+import { AirbnbRating } from 'react-native-ratings';
+import { RootStackScreenEnumExplore } from '../../component/Root/RootStackExplore';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { RootStackParamListHome, RootStackScreenEnumHome } from '../../Root/RootStackHome';
 
 
 interface Category {
@@ -28,11 +31,42 @@ interface Category {
 }
 type BottomNavigationProp = CompositeNavigationProp<NavigationProp<RootTabParamList>, StackNavigationProp<RootStackParamListHome, RootStackScreenEnumHome>>;
 const ExploreScreen = ({ navigation }: NativeStackHeaderProps | any) => {
-  
+  const isFocused = useIsFocused();
+
+  const navigationBottom = useNavigation<BottomNavigationProp>();
+
+  const [textInputStatus, setTextInputStatus] = useState<boolean>(false);
+
+  const [textInputSearch, setTextInputSearch] = useState<string>('');
+
+  const [listCategory, setListCategory] = useState<[]>([]);
+
+  const fetchListCategory = async () => {
+    const response = await AxiosInstance().get('category/getAllCategory');
+    setListCategory(response.data);
+  }
+  useEffect(() => {
+    if (isFocused) {
+      fetchListCategory();
+    }
+  }, [isFocused])
+
+  const [refreshingCategory, setRefreshingCategory] = useState<boolean>(false);
+
+  const onRefreshCategory = React.useCallback(() => {
+    setRefreshingCategory(true);
+    fetchListCategory();
+    setTimeout(() => {
+      setRefreshingCategory(false);
+    }, 2000);
+  }, []);
 
   const renderItem = ({ item }: any): React.JSX.Element => {
     return (
       <TouchableOpacity
+        onPress={() => {
+          navigation.navigate(RootStackScreenEnumExplore.Category_Detail_Screen, { categoryID: item._id });
+        }}
         style={styles.containerItemPD}
       >
         <View>
@@ -50,25 +84,37 @@ const ExploreScreen = ({ navigation }: NativeStackHeaderProps | any) => {
   return (
     <View style={styles.container} >
       <View style={styles.top}>
-        <View style={styles.headerLeft} >
+        <View style={(!textInputStatus) ? styles.headerLeft : [styles.headerLeft, { borderColor: COLORS.gray }]}>
           <Icon name='search' size={22} />
           <TextInput
             placeholder="Tìm kiếm"
             style={[styles.TextSearch]}
+            onFocus={() => setTextInputStatus(true)}
+            onBlur={() => setTextInputStatus(false)}
+            onChangeText={setTextInputSearch}
+            value={textInputSearch}
           />
+          {(textInputStatus) ? (
             <Pressable style={{ position: 'absolute', right: 5, backgroundColor: '#dbd9d9', borderRadius: 5 }}
+              onPress={() => setTextInputSearch('')}
             >
+              <Icon name='close' size={14} />
             </Pressable>
+          )
+            : null}
         </View>
 
         <View style={styles.headerRight}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => navigationBottom.navigate(RootStackScreenEnumHome.NotificationScreen)}>
             <Icon name="notifications-outline" size={25} />
           </TouchableOpacity>
         </View>
       </View>
       <FlatList
-        data={[]}
+        refreshControl={
+          <RefreshControl refreshing={refreshingCategory} onRefresh={onRefreshCategory} />
+        }
+        data={listCategory.filter((item: any) => item.name.toLowerCase().includes(textInputSearch.toLowerCase()))}
         renderItem={renderItem}
         numColumns={2}
         keyExtractor={(item: any) => item?._id.toString()} />
